28699a10f6cf7325bd1eb6cb4a774dc468a3cc68
Pranav Moktali <pmoktali@enova.com>
2016-12-15 16:09:09 -0600

    updates style as per autopep8

--- mnist/beginner_mnist.py
+++ mnist/beginner_mnist.py
@@ -4,12 +4,14 @@ import tensorflow as tf
 from arg_parser import *
 
 OPTIMIZERS_NAME_TO_CLASS_MAP = {
-    'GradientDescent' : tf.train.GradientDescentOptimizer(0.3),
-    'Adagrad' : tf.train.AdagradOptimizer(0.3),
-    'Adadelta' : tf.train.AdadeltaOptimizer(0.3),
-    'ProximalAdagrad' : tf.train.ProximalAdagradOptimizer(0.3),
-    'ProximalGradientDescent' : tf.train.ProximalGradientDescentOptimizer(0.3)
+    'GradientDescent': tf.train.GradientDescentOptimizer(0.3),
+    'Adagrad': tf.train.AdagradOptimizer(0.3),
+    'Adadelta': tf.train.AdadeltaOptimizer(0.3),
+    'ProximalAdagrad': tf.train.ProximalAdagradOptimizer(0.3),
+    'ProximalGradientDescent': tf.train.ProximalGradientDescentOptimizer(0.3)
 }
+
+
 def main(optimizers):
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -23,7 +25,8 @@ def main(optimizers):
 
     # Training
     y_ = tf.placeholder(tf.float32, [None, 10])
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(y, y_))
     train_steps = []
 
     for optimizer in optimizers:
@@ -43,10 +46,15 @@ def main(optimizers):
         for i in range(1000):
             batch_xs, batch_ys = mnist.train.next_batch(100)
             sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        printable_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-        print('Optimizer: ' + str(train_step.name) + ', Accuracy: ' + str(printable_accuracy))
+        printable_accuracy = sess.run(
+            accuracy,
+            feed_dict={
+                x: mnist.test.images,
+                y_: mnist.test.labels})
+        print('Optimizer: ' + str(train_step.name) +
+              ', Accuracy: ' + str(printable_accuracy))
 
 if __name__ == '__main__':
     optimizers = arg_parser()
